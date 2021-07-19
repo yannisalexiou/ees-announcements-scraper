89f7eb955306602988f3eec2efac857af1fa9bbd
yannisalexiou <johnmast7@yahoo.com>
2021-07-20 01:13:18 +0300

    EES Site Updated and scraped not working closed #1

--- announcement/index.js
+++ announcement/index.js
@@ -2,46 +2,39 @@ const cheerio = require("cheerio");
 const fetch = require("node-fetch"); //Helps us make HTTP calls
 
 //All Announcements
-const allAnnouncementsSelector = "div#claroBody";
-const eachAnnouncementSelector = "div#claroBody>table";
+const allAnnouncementsSelector = "div#claroBody>div";
+const eachAnnouncementSelector = "div#claroBody>div>div:nth-child(3)";
 
 //Link
 const announcementsLocation = "http://mycourses.ntua.gr/announcements/announcements.php?cidReq=PSTGR1083";
 
-function findAnnouncementsTest(tableSelector, html) {
+function findAnnouncementsTest(announcementSelector, html) {
     var $ = cheerio.load(html);
-    var announcements = $(tableSelector).filter(function () {
+    var announcements = $(announcementSelector).filter(function () {
         var data = $(this);
         return data;
     });
     return announcements.text();
 }
 
-function findAnnouncements(tableSelector, html) {
+function findAnnouncements(announcementSelector, html) {
+    debugger;
     var $ = cheerio.load(html);
-    var announcements = $(tableSelector).filter(function () {
+    var announcements = $(announcementSelector).filter(function () {
         var data = $(this);
         return data;
     });
 
     var allAnnouncement = [];
 
-    announcements.children("table[cellspacing='0']").each(function () {
+    announcements.children().each(function () {
         var data = $(this);
         //Each Selector
-        var title = data.children().children().children().children().eq(1).text();
-        var date = data.children().children().children().children().eq(3).text();
-        var text = "";
-
-        data.nextUntil("table").each(function() {
-            var currentString = $(this).text();
-            if(!/\s+$/.test(currentString)) {
-                //Add space between each element
-                
-                currentString += " ";
-            }
-            text += currentString;
-        });
+        var title = data.children().eq(0).text()
+        var date = data.children().eq(1).text()
+        var text = data.children().eq(2).text()
+        var htmlText = data.children().eq(2).html()
+        
         //Remove the last char if is the space.
         if (/\s+$/.test(text)) {
             text = text.slice(0, -1);
@@ -53,7 +46,7 @@ function findAnnouncements(tableSelector, html) {
         text = text.replace(/^\s/, '');
 
         //Make Object
-        var eachItem = {title, date, text};
+        var eachItem = {title, date, text, htmlText};
         allAnnouncement.push(eachItem);
     });
     return allAnnouncement;
